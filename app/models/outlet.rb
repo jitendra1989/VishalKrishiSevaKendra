8a06df1b8ae061d0b29cfa93270dad6db06efe7b
Arlston Fernandes <archferns@gmail.com>
2015-07-12 23:25:29 +0530

    Add condition to not show superadmins as Outlet users

--- app/models/outlet.rb
+++ app/models/outlet.rb
@@ -1,4 +1,4 @@
 class Outlet < ActiveRecord::Base
-	has_many :users
+	has_many :users, -> { where 'role != ?', 'super_admin' }
 	[:name, :city, :state, :country].each { |n| validates n, presence: true }
 end
--- spec/models/outlet_spec.rb
+++ spec/models/outlet_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Outlet, type: :model do
 	let(:outlet) { FactoryGirl.build(:outlet) }
+	let(:superadmin) { FactoryGirl.create(:super_admin) }
 	it { expect(outlet).to be_valid }
 	it { expect(outlet).to respond_to(:users) }
+	it { expect(superadmin.outlet.users).not_to include(superadmin) }
 	it "has a valid name" do
 		outlet.name = nil
 		expect(outlet).to_not be_valid

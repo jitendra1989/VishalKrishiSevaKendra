741e2fb19868713cd5b78ab7ae3e2953cc6bab11
Arlston Fernandes <archferns@gmail.com>
2015-07-24 00:52:24 +0530

    Automatically update user permissions if role is updated

--- app/models/role_permission.rb
+++ app/models/role_permission.rb
@@ -1,4 +1,12 @@
 class RolePermission < ActiveRecord::Base
   belongs_to :role
   belongs_to :permission
+  after_save :set_user_permissions
+
+  private
+    def set_user_permissions
+      self.role.users.each do |user|
+        user.permissions << self.permission
+      end
+    end
 end
--- spec/models/role_permission_spec.rb
+++ spec/models/role_permission_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe RolePermission, type: :model do
+	let(:user) { FactoryGirl.create(:user) }
 	let(:role_permission) { FactoryGirl.build(:role_permission) }
 	it { expect(role_permission).to be_valid }
 	it { expect(role_permission).to respond_to(:role) }
 	it { expect(role_permission).to respond_to(:permission) }
+
+	describe 'automatic update of user permissions' do
+		before do
+			user.roles << role_permission.role
+			role_permission.save!
+		end
+		it 'assigns the same permission to user' do
+			role_permission.role.users.each do |user|
+				expect(user.permissions).to include(role_permission.permission)
+			end
+		end
+	end
 end

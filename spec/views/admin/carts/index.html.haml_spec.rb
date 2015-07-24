3a78d6f605dcc41ce0d19820ca51469a5f749512
Arlston Fernandes <archferns@gmail.com>
2015-07-24 22:06:13 +0530

    Add carts index page

--- spec/views/admin/carts/index.html.haml_spec.rb
+++ spec/views/admin/carts/index.html.haml_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "carts/index.html.haml", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "admin/carts/index", type: :view do
+
+	let(:carts) { Cart.all }
+
+	it "renders attributes in <p>" do
+		assign(:carts, carts)
+		render
+		expect(rendered).to include('Facilitator')
+	end
 end

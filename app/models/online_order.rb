4861c340adc019982128d6cd94e7fb33e01cf7b4
Arlston Fernandes <archferns@gmail.com>
2015-07-28 23:36:07 +0530

    Add onlinecart to order flow

--- app/models/online_order.rb
+++ app/models/online_order.rb
@@ -1,4 +1,34 @@
 class OnlineOrder < ActiveRecord::Base
   belongs_to :customer
   has_many :items, class: OnlineOrderItem, dependent: :destroy
+
+  attr_accessor :online_cart_id
+
+  validates :customer_id, presence: true
+
+  after_initialize :add_customer, if: :new_record?
+  after_create :add_cart_items
+
+  private
+    def add_customer
+      self.customer = OnlineCart.find(self.online_cart_id).customer if self.online_cart_id
+    end
+
+    def add_cart_items
+      if self.online_cart_id
+        online_cart = OnlineCart.includes(:items).find(self.online_cart_id)
+        online_cart.items.includes(:product).each do |cart_item|
+          self.items.create(product: cart_item.product, quantity: cart_item.quantity)
+          cart_item.product.stocks.where(outlet: Outlet.online_outlets.ids).group('outlet_id desc').each do |stock|
+            if stock.online_carts.try(:include?, self.online_cart_id.to_s)
+              stock.ordered += stock.online_carts[self.online_cart_id.to_s]
+              stock.quantity -= stock.online_carts[self.online_cart_id.to_s]
+              stock.save!
+            else
+            end
+          end
+        end
+        online_cart.destroy!
+      end
+    end
 end
--- spec/models/online_order_spec.rb
+++ spec/models/online_order_spec.rb
@@ -5,4 +5,40 @@ RSpec.describe OnlineOrder, type: :model do
   it { expect(online_order).to be_valid }
   it { expect(online_order).to respond_to(:customer) }
   it { expect(online_order).to respond_to(:items) }
+
+  it "has a valid customer" do
+    online_order.customer = nil
+    expect(online_order).not_to be_valid
+  end
+
+  describe 'after online_order creation' do
+    let!(:stock) { FactoryGirl.create(:stock, outlet: FactoryGirl.create(:online_outlet)) }
+    let(:online_cart) { FactoryGirl.create(:online_cart) }
+    before do
+      online_order.online_cart_id = online_cart.id
+      online_cart.add_item(stock.product.id, stock.quantity)
+    end
+    it 'adds all cart products to itself' do
+      cart_items_count = online_cart.items.size
+      expect{
+        online_order.save!
+      }.to change(OnlineOrderItem, :count).by(cart_items_count)
+    end
+    it 'destroys the existing cart' do
+      expect{
+        online_order.save!
+      }.to change(OnlineCart, :count).by(-1)
+    end
+    describe 'stock control' do
+      it 'adds the requested quantity to ordered' do
+        online_order.save!
+        expect(stock.reload.ordered).to eq(online_order.items.find_by(product: stock.product).quantity)
+      end
+      it 'subtracts the requested quantity from quantity' do
+        stock_quantity = stock.quantity
+        online_order.save!
+        expect(stock.reload.quantity).to eq(stock_quantity - online_order.items.find_by(product: stock.product).quantity)
+      end
+    end
+  end
 end

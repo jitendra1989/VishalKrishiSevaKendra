97803f0343821f0bc5ad526d624eb049f968e767
Arlston Fernandes <archferns@gmail.com>
2015-08-11 11:45:54 +0530

    Add payment gateway related integration info

--- spec/views/front/orders/new.html.haml_spec.rb
+++ spec/views/front/orders/new.html.haml_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "front/orders/new", type: :view do
 	let(:cart) { FactoryGirl.create(:online_cart) }
+	let(:current_customer) { FactoryGirl.create(:customer) }
 
 	it "renders cart info" do
 		assign(:cart, cart)
 		assign(:order, OnlineOrder.new)
+		assign(:current_customer, current_customer)
 		render
 		expect(rendered).to include('Order')
 	end

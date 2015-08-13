76549e0615125e3574a747eb82e6e1f0416107a2
Arlston Fernandes <archferns@gmail.com>
2015-08-13 14:20:27 +0530

    Add large decorated price helper

--- app/helpers/front/products_helper.rb
+++ app/helpers/front/products_helper.rb
@@ -1,4 +1,13 @@
 module Front::ProductsHelper
+	def large_decorated_frontend_price(product)
+		if product.price_with_online_taxes > product.online_price_with_taxes
+			discount_amount = product.price_with_online_taxes - product.sale_price_with_online_taxes
+			discount_percent = (discount_amount*100/product.price_with_online_taxes).round
+			"<span><s class='h4'>Price: #{number_to_currency product.price_with_online_taxes}</s><br/><br/><span class='h3'>Offer Price:#{number_to_currency product.sale_price_with_online_taxes} (#{discount_percent}% Off)</span></span>".html_safe
+		else
+			"<span class='h3'>#{number_to_currency product.price_with_online_taxes}</span>".html_safe
+		end
+	end
 	def decorated_frontend_price(product)
 		if product.price_with_online_taxes > product.online_price_with_taxes
 			"<span><s>#{number_to_currency product.price_with_online_taxes}</s>#{number_to_currency product.sale_price_with_online_taxes}</span>".html_safe
--- spec/helpers/front/products_helper_spec.rb
+++ spec/helpers/front/products_helper_spec.rb
@@ -14,4 +14,21 @@ RSpec.describe Front::ProductsHelper, type: :helper do
 			expect(helper.decorated_frontend_price(product)).to include(number_to_currency product.sale_price_with_online_taxes)
 		end
 	end
+	describe "large decorated frontend_price" do
+		it "returns the regular price if no sale price is provided" do
+			product.sale_price = 0
+			expect(helper.large_decorated_frontend_price(product)).to include(number_to_currency product.price_with_online_taxes)
+		end
+		it "returns the regular price with a strikeout class" do
+			expect(helper.large_decorated_frontend_price(product)).to include(number_to_currency product.price_with_online_taxes)
+		end
+		it "returns the sale price with taxes" do
+			expect(helper.large_decorated_frontend_price(product)).to include(number_to_currency product.sale_price_with_online_taxes)
+		end
+		it "returns the sale discount amount" do
+			discount_amount = product.price_with_online_taxes - product.sale_price_with_online_taxes
+			discount_percent = discount_amount*100/product.price_with_online_taxes
+			expect(helper.large_decorated_frontend_price(product)).to include(discount_percent.round.to_s)
+		end
+	end
 end

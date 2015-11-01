f4a4ce196e7f912b3157cfeea34534c1cec2bf1b
Arlston Fernandes <archferns@gmail.com>
2015-11-02 00:05:37 +0530

    Add table styling to invoices

--- spec/views/admin/invoices/index.html.haml_spec.rb
+++ spec/views/admin/invoices/index.html.haml_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "invoices/index.html.haml", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "admin/invoices/index", type: :view do
+
+	let(:invoices) { Invoice.all.page(1) }
+
+	it "renders attributes in <p>" do
+		assign(:invoices, invoices)
+		render
+		expect(rendered).to match(/Invoice No/)
+		expect(rendered).to match(/Invoice Date/)
+		expect(rendered).to match(/Amount/)
+		expect(rendered).to match(/Receipts/)
+	end
 end

1759cb5fbc2c18522bc2a9abb3657df054f1346f
Arlston Fernandes <archferns@gmail.com>
2015-11-16 22:36:26 +0530

    Fix error on new quotation page

--- spec/views/admin/quotations/new.html.haml_spec.rb
+++ spec/views/admin/quotations/new.html.haml_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "admin/quotations/new", type: :view do
   let(:customer) { FactoryGirl.create(:customer) }
+  let(:user) { FactoryGirl.create(:user) }
 
   it "renders new quotation form" do
     assign(:quotation, customer.quotations.new(user: FactoryGirl.create(:user)))
     assign(:customer, customer)
+    assign(:current_user, user)
     render
     assert_select "form[action=?][method=?]", admin_customer_quotations_path(customer), "post" do
       assert_select "input#quotation_discount_amount[name=?]", "quotation[discount_amount]"

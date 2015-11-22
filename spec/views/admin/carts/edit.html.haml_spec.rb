36298900d84cea0b1147f1956e9ebe09565f981b
Arlston Fernandes <archferns@gmail.com>
2015-11-23 01:13:25 +0530

    Allow discount based on current user

--- spec/views/admin/carts/edit.html.haml_spec.rb
+++ spec/views/admin/carts/edit.html.haml_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "admin/carts/edit", type: :view do
   let(:cart) { FactoryGirl.create(:cart) }
+  let(:user) { FactoryGirl.create(:user) }
 
   it "renders attributes in <p>" do
     assign(:cart, cart)
     assign(:order, Order.new)
+    assign(:current_user, user)
     render
     expect(rendered).to match(/Product/)
     expect(rendered).to match(/Price/)

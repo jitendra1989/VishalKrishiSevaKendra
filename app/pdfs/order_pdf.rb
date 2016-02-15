d9b200d83e0bd8676595c9c37d3ef4eaf17c2436
Arlston Fernandes <archferns@gmail.com>
2016-02-16 02:38:56 +0530

    Format order pdf

--- app/pdfs/order_pdf.rb
+++ app/pdfs/order_pdf.rb
@@ -8,9 +8,9 @@ class OrderPdf < Prawn::Document
 			font "Helvetica"
 			text_box "#{order.customer.name}" ,at:[10,50] , size: 12, style: :bold
 			move_down 5
-			text_box "#{order.customer.full_address}" ,at:[10,36] , size: 10
+			text_box "#{order.customer.full_address}" ,at:[10,36], size: 10
 			move_down 5
-			text_box "Phone #{order.customer.mobile}" ,at:[10,22] , size: 10
+			text_box "Phone #{order.customer.mobile}" ,at:[10,22], size: 10
 			text_box "Number :#{order.id}", at: [400, 50], size: 10
 			text_box "Date :#{order.customer.created_at.strftime('%d-%m-%Y')}", at: [400, 36], size: 10
 			text_box "Delivery Date :#{order.customer.created_at.strftime('%d-%m-%Y')}", at: [400, 22], size: 10
@@ -26,25 +26,24 @@ class OrderPdf < Prawn::Document
 		text_box "for Damian de Goa", style: :bold, at: [420, 420 - (20 * order.items.size)]
 		text_box "Authorized Signatory", at: [420, 360 - (20 * order.items.size)]
 		text_box "TERMS & CONDITIONS FOR BUSINESS", style: :bold, at: [0, 400 - (20 * order.items.size)]
-		text_box "PAYMENT TERMS", at: [0, 340 - (20 * order.items.size)]
-		text_box "50% Advance Against Order.", at: [0, 320 - (20 * order.items.size)]
-		text_box "50% Balance Payments Before Delivery.", at: [0, 300 - (20 * order.items.size)]
-		text_box "In case of cheque payment, delivery will be", at: [0, 280 - (20 * order.items.size)]
-		text_box "after realization of the cheque.", at: [0, 260 - (20 * order.items.size)]
-		text_box "In case of cancellation of order, advance", at: [0, 240 - (20 * order.items.size)]
-		text_box "received is non refundable.", at: [0, 220 - (20 * order.items.size)]
-		text_box "Excise Duty, VAT, service tax will be charged extra", at: [0, 200 - (20 * order.items.size)]
-		text_box "if and as is applicable at the time of delivery.", at: [0, 180 - (20 * order.items.size)]
-		text_box "Fabric, Polish shade, design and dimension will not be changed once the order is confirmed.", at: [0, 160 - (20 * order.items.size)]
-		text_box "All the disputes are subjected to Panjim jurisdiction.", at: [0, 140 - (20 * order.items.size)]
-		text_box "Above terms and conditions are approved by me.", at: [0, 120 - (20 * order.items.size)]
-		text_box "Client Signature", style: :bold, at: [0, 60 - (20 * order.items.size)]
+		text_box "PAYMENT TERMS", at: [0, 340 - (20 * order.items.size)], size: 10
+		text_box "50% Advance Against Order.", at: [0, 325 - (20 * order.items.size)], size: 10
+		text_box "50% Balance Payments Before Delivery.", at: [0, 310 - (20 * order.items.size)], size: 10
+		text_box "In case of cheque payment, delivery will be", at: [0, 295 - (20 * order.items.size)], size: 10
+		text_box "after realization of the cheque.", at: [0, 280 - (20 * order.items.size)], size: 10
+		text_box "In case of cancellation of order, advance", at: [0, 265 - (20 * order.items.size)], size: 10
+		text_box "received is non refundable.", at: [0, 250 - (20 * order.items.size)], size: 10
+		text_box "Excise Duty, VAT, service tax will be charged extra", at: [0, 235 - (20 * order.items.size)], size: 10
+		text_box "if and as is applicable at the time of delivery.", at: [0, 220 - (20 * order.items.size)], size: 10
+		text_box "Fabric, Polish shade, design and dimension will not be changed once the order is confirmed.", at: [0, 205 - (20 * order.items.size)], size: 10
+		text_box "All the disputes are subjected to Panjim jurisdiction.", at: [0, 190 - (20 * order.items.size)], size: 10
+		text_box "Above terms and conditions are approved by me.", at: [0, 175 - (20 * order.items.size)], size: 10
+		text_box "Client Signature", style: :bold, at: [0, 80 - (20 * order.items.size)]
 	end
 	def line_items(order)
 		table line_item_rows(order), width: 540 do
 			row(0).font_style = :bold
 			columns(1..4).align = :right
-			self.row_colors = ["DDDDDD", "FFFFFF"]
 			self.header = true
 		end
 	end

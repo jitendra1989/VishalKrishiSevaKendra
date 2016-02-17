d0301f247f6072c84d34aee2bb7e4eb0184a29fc
Arlston Fernandes <archferns@gmail.com>
2016-02-18 02:54:47 +0530

    Remove - on discount percentage

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -12,7 +12,7 @@ module Admin::ProductsHelper
 		if product.price_with_taxes > product.final_price_with_taxes
 			discount_amount = product.price_with_taxes - product.sale_price_with_taxes
 			discount_percent = (discount_amount*100/product.price_with_taxes).round
-			"<span><s>#{number_to_currency product.price_with_taxes}</s>#{number_to_currency product.sale_price_with_taxes}</span><span class='discount-percent'>-#{discount_percent}%</span>".html_safe
+			"<span><s>#{number_to_currency product.price_with_taxes}</s>#{number_to_currency product.sale_price_with_taxes}</span><span class='discount-percent'>#{discount_percent}%</span>".html_safe
 		else
 			"<span>#{number_to_currency product.price_with_taxes}</span>".html_safe
 		end
--- app/helpers/front/products_helper.rb
+++ app/helpers/front/products_helper.rb
@@ -12,7 +12,7 @@ module Front::ProductsHelper
 		if product.price_with_online_taxes > product.final_online_price_with_taxes
 			discount_amount = product.price_with_online_taxes - product.sale_price_with_online_taxes
 			discount_percent = (discount_amount*100/product.price_with_online_taxes).round
-			"<span><s>#{number_to_currency product.price_with_online_taxes}</s>#{number_to_currency product.sale_price_with_online_taxes}</span><span class='discount-percent'>-#{discount_percent}%</span>".html_safe
+			"<span><s>#{number_to_currency product.price_with_online_taxes}</s>#{number_to_currency product.sale_price_with_online_taxes}</span><span class='discount-percent'>#{discount_percent}%</span>".html_safe
 		else
 			"<span>#{number_to_currency product.price_with_online_taxes}</span>".html_safe
 		end
